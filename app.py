080f3953f476c05da8d4f5f48ca63ba1de715969
beagle529 <beagle0529@gmail.com>
2025-01-22 16:58:40 +0800

    更新支援IPHONE

--- app.py
+++ app.py
@@ -90,6 +90,7 @@ home_template = """
 <html lang="zh-Hant">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dr.Veggie V.15</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   
@@ -144,6 +145,7 @@ challenge_template = """
 <html lang="zh-Hant">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>第{{ level }}關 v16</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   
